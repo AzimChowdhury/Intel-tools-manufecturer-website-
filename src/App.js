9389a0898c7bf9ed3d01068cee46739579158e47
Azim Chowdhury <azimchy994@gmail.com>
2022-05-23 13:44:20 +0600

    products loaded

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
 import Navbar from "./Home/Navbar";
 
 
@@ -5,7 +7,9 @@ function App() {
   return (
     <div>
       <Navbar></Navbar>
-
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Slider from './Slider';
+import Products from './Products';
+import Summary from './Summary';
+
+function Home() {
+    return (
+        <div>
+            <Slider></Slider>
+            <Products></Products>
+            <Summary></Summary>
+        </div>
+    )
+}
+
+export default Home
