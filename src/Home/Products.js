4c57ecc0a31396e048ec08052a47f5c15cd6c917
Azim Chowdhury <azimchy994@gmail.com>
2022-05-23 18:18:00 +0600

    business sumarry

--- src/Home/Products.js
+++ src/Home/Products.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 // import { useQuery } from 'react-query';
 
 
@@ -7,25 +8,27 @@ const Products = () => {
     // const { data } = useQuery('items', fetch('processor.json').then(res => res.json()))
     const [products, setProducts] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch('https://intel-server-azim.herokuapp.com/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
 
     return (
         <div>
+            <h1 className='mt-16 text-3xl text-center'>Choose the best processor for your company</h1>
             <div className='grid lg:grid-cols-2 sm:grid-cols-1 gap-4 justify-items-center mt-12'>
                 {
                     products.map(product => <>
-                        <div class="card card-side max-w-xl bg-base-100 mb-4 shadow-2xl">
-                            <figure className='p-4'><img src={product.image} alt="intel" /></figure>
+                        <div class="card card-side max-w-xl max-h-96 bg-base-100 mb-4 shadow-2xl">
+                            <figure className='p-4 w-96'><img src={product.image} alt="intel" /></figure>
                             <div class="card-body">
-                                <h2 class="card-title text-2xl">{product.name}</h2>
+                                <h2 class="card-title font-bold text-2xl">{product.name}</h2>
                                 <p><small>{product.description}</small></p>
-                                <p className='py-1 m-0'>Available: {product.available} piece</p>
-                                <p  className='py-1 m-0'>Price: $ {product.price}</p>
+                                <p className='p-0 m-0'>Available: <span className='font-bold'>{product.available} </span> piece</p>
+                                <p className='p-0 m-0'>Minimum order: <span className='font-bold'>{product.minimum} </span>  piece</p>
+                                <p className='p-0 m-0'>Price:<span className='font-bold'> $ {product.price} </span> </p>
                                 <div class="card-actions justify-start">
-                                    <button class="btn btn-primary text-white">Purchase</button>
+                                    <button class="btn btn-sm btn-primary text-white">Purchase</button>
                                 </div>
                             </div>
                         </div>
--- src/Home/Summary.js
+++ src/Home/Summary.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 function Summary() {
+    const data = [
+        {img:'https://i.ibb.co/WKBm4bq/download-removebg-preview.png', name:"50K+ Customers",details:"Worldwide millions of user using Intel processor and thousends of company building there pc with Intel processors"},
+        {img:'https://i.ibb.co/nR1gnzX/review-icon-png-14-removebg-preview.png', name:"50M+ Annual Revenue ",details:"Intel is rapidly growing there user number  and producing over 50 million revenue worldwide"},
+        {img:'https://i.ibb.co/djv1KSJ/processor-circuit-isolated-icon-vector-18795995-removebg-preview.png', name:"100+ products",details:"Intel has more than 100 types of processor with various generation."}
+    ]
     return (
-        <div>
-            <h1>Business Summary</h1>
+        
+        <div className='m-20 mx-32'>
+             <h1 className='text-3xl text-center m-10'>Overview</h1>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-8 justify-items-center'>
+                {
+                    data.map(d=>
+                    <div class="card w-72 bg-base-100 shadow-2xl">
+                    <figure class="px-10 pt-10">
+                        <img className='w-full' src={d.img} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title">{d.name}</h2>
+                        <p>{d.details}</p>
+                    </div>
+                    </div>)
+                }
+            </div>  
         </div>
     )
 }
